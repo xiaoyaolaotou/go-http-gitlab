832b131a5311bed8c7ff59a25d18b2fe48362133
Your Name <you@example.com>
2020-12-08 11:16:23 +0800

    go-http-page

---
+++ gitlabv2.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// Gitlab 结构体
+type Gitlab struct {
+	Id                  int    `json:"id"`
+	Path_with_namespace string `json:"path_with_namespace"`
+}
+
+var totalPages uint
+var slicestr = make([]int, 0)
+
+// 打印Body中的数据
+func printBody(r *http.Response) {
+
+	if r.Body != nil {
+		defer r.Body.Close()
+	}
+
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var gitlab []Gitlab
+	if err := json.Unmarshal(content, &gitlab); err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, v := range gitlab {
+		slicestr = append(slicestr, v.Id)
+		//fmt.Println(k, v.Id, v.Path_with_namespace)
+	}
+	fmt.Println(slicestr)
+	/*
+	 */
+}
+
+// requestByParams 获取所有项目信息
+func requestByParams(page string) *http.Response {
+	request, err := http.NewRequest("GET", "http://code.com.com/gitlab/api/v4/projects", nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	request.Header.Add("PRIVATE-TOKEN", "ve9ifEu9ZtAeqWik1L")
+	params := make(url.Values)
+	params.Add("page", page)
+	params.Add("membership", "true")
+
+	//fmt.Println(params.Encode())           // 打印GET参数
+	request.URL.RawQuery = params.Encode() // params.encode就是GET请求的参数进行URL解析
+	r, err := http.DefaultClient.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return r
+	// 关闭Body
+
+	// 打印相关数据
+	//printBody(r)
+
+}
+
+// 获取项下目的分支
+
+func main() {
+	r := requestByParams("1")
+	printBody(r)
+	// 取总页数
+	totalPages := r.Header.Get("X-Total-Pages")
+	nPage, err := strconv.Atoi(totalPages)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//获取以后页数
+	for i := 2; i <= nPage; i++ {
+		r = requestByParams(strconv.Itoa(i))
+		printBody(r)
+	}
+
+}
--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/url"
-
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 // Gitlab 结构体
@@ -17,6 +17,20 @@ type Gitlab struct {
 
 // 打印Body中的数据
 func printBody(r *http.Response) {
+	totalPages := r.Header.Get("X-Total-Pages")       // 取总页数
+	page := fmt.Sprintf("%d", r.Header.Get("X-Page")) // 取当前页数
+	n_totalPages, _ := strconv.Atoi(totalPages)
+	n_page, _ := strconv.Atoi(page)
+
+	for {
+		if n_page < n_totalPages {
+			fmt.Println(n_page)
+			n_page += 1
+			requestByParams(strconv.Itoa(n_page))
+		} else if n_page == n_totalPages {
+			break
+		}
+	}
 
 	content, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -26,7 +40,7 @@ func printBody(r *http.Response) {
 
 	var gitlab []Gitlab
 	if err := json.Unmarshal(content, &gitlab); err != nil {
-		fmt.Println(err, "xxxx")
+		fmt.Println(err)
 		return
 	}
 	for k, v := range gitlab {
@@ -35,17 +49,20 @@ func printBody(r *http.Response) {
 
 }
 
-func requestByParams() {
-	request, err := http.NewRequest("GET", "http://code.com.com/gitlab/api/v4/projects", nil)
+func requestByParams(page string) {
+	request, err := http.NewRequest("GET", "http://code.gome.inc/gitlab/api/v4/projects", nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	request.Header.Add("private-token", "ve9ifEu9ZtAeqWik1L")
+	request.Header.Add("PRIVATE-TOKEN", "ve9ifEu9ZtAeqWik1L_y")
 	params := make(url.Values)
+	//params.Add("per_page", per_page)
+	params.Add("page", page)
 	params.Add("membership", "true")
-	//fmt.Println(params.Encode())
-	request.URL.RawQuery = params.Encode() // params.encode就是GET请求的参数
+
+	fmt.Println(params.Encode())           // 打印GET参数
+	request.URL.RawQuery = params.Encode() // params.encode就是GET请求的参数进行URL解析
 	r, err := http.DefaultClient.Do(request)
 	if err != nil {
 		fmt.Println(err)
@@ -54,12 +71,13 @@ func requestByParams() {
 	// 关闭Body
 	defer r.Body.Close()
 
+	// 打印相关数据
 	printBody(r)
 
 }
 
 func main() {
 
-	requestByParams()
+	requestByParams("1")
 
 }
